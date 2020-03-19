fd056f11c40f69f064afd4099371a624b7ada677
RodrigoLMcGill <rodrigo.lisboamirco@mail.mcgill.ca>
2020-03-19 18:48:15 -0400

    Added sign-up url

--- Marketplace/Marketplace/urls.py
+++ Marketplace/Marketplace/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('signup/', users_views.signup),
 ]
--- Marketplace/users/views.py
+++ Marketplace/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def signup(request):
+    return HttpResponse('<h1>Sign-up page</h1>')
