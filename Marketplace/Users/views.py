2a9b82756e4afec5554c5fa03eaac11c74d94a05
RodrigoLMcGill <rodrigo.lisboamirco@mail.mcgill.ca>
2020-03-21 16:43:57 -0400

    Added functional signup form page

--- Marketplace/Users/views.py
+++ Marketplace/Users/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.models import User
+from . import forms
+from django.db import IntegrityError
+
 
 # Create your views here.
 
 
 def signup(request):
-    return HttpResponse('<h1>Sign-up page</h1>')
+    context = {}
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
+        if form.is_valid():
+            try:
+                User.objects.create_user(
+                    username=form.cleaned_data['username'],
+                    email=form.cleaned_data['email'],
+                    password=form.cleaned_data['password'],
+                    first_name=form.cleaned_data['first_name'],
+                    last_name=form.cleaned_data['last_name'])
+
+                return render(request, 'Market/home.html')
+            except IntegrityError:
+                form.add_error('username', 'Username is taken')
+        context['form'] = form
+
+    return render(request, 'Users/signup.html', context)
