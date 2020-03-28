3e7413a430dd6f42e7772f58b350de99847216e4
Olivier Grenier-Leboeuf <olivier.grenier.leboeuf@gmail.com>
2020-03-27 22:18:23 -0400

    Changed URL config

---
+++ Marketplace/Users/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+  path('signup/', views.signup, name='signup'),
+  path('login/', views.do_login, name='login'),
+  path('logout/', views.do_logout, name='logout'),
+]
