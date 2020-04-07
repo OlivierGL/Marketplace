020c162091f9bcd576a60705e4bf3dcbc476803c
Olivier Grenier-Leboeuf <olivier.grenier.leboeuf@gmail.com>
2020-04-07 16:45:15 -0400

    added add product functionality and other good stuff

---
+++ Marketplace/Market/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from . import models
+
+class AddProductForm(forms.ModelForm):
+	class Meta:
+		model = models.Product
+		fields = ['name', 'description', 'quantity', 'price', 'image', 'category']
+
+	
