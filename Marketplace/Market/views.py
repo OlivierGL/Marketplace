093fb1b8cfcf7574af875de8150237ad3c5b462b
RodrigoLMcGill <rodrigo.lisboamirco@mail.mcgill.ca>
2020-04-16 14:07:11 -0400

    Remove home page and added All products option to Browse nav

--- Marketplace/Market/views.py
+++ Marketplace/Market/views.py
@@ -4,7 +4,6 @@ from . import models
 from . import forms
 from Users import models as user_models
 from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.contrib import messages
 
 no_product_error_message = "Sorry, no {} are available for now."
@@ -12,43 +11,16 @@ no_product_error_message = "Sorry, no {} are available for now."
 
 # Create your views here.
 def home(request):
-    return render(request, 'Market/home.html', {'activeNavItem': 'home'})
+    context = {
+        'category': "all products",
+        'items': models.Product.objects.filter(quantity__gt=0),
+        'activeNavItem': "browse/all",
+        'noProductErrorMessage': no_product_error_message.format("products")
+    }
+    return render(request, 'Market/browse.html', context)
 
 
 def paintings(request):
-    # made up item to demo the browser template.
-    items = [{
-        'pk': 1,
-        'name': 'The Creation of the Sun and the Moon',
-        'description': "It is one of the frescoes from Michelangelo's nine Books of Genesis scenes on the Sistine Chapel ceiling.",
-        'quantity': '3',
-        'image': 'Market/thecreation.jpg',
-        'price': '4300'}, {
-        'pk': 2,
-        'name': 'Self-portrait',
-        'description': "Most probably, van Gogh's self-portraits are depicting the face as it appeared in the mirror he used to reproduce his face, i.e. his right side in the image is in reality the left side of his face.",
-        'quantity': '2',
-        'image': 'Market/selfportrait.jpg',
-        'price': '2000'}, {
-        'pk': 3,
-        'name': 'Mona Lisa',
-        'description': "The best known, the most visited, the most written about, the most sung about, the most parodied work of art in the world",
-        'quantity': '7',
-        'image': 'Market/monalisa.jpg',
-        'price': '1864'}, {
-        'pk': 4,
-        'name': 'The Scream',
-        'description': "The agonised face in the painting has become one of the most iconic images of art, seen as symbolising the anxiety of the human condition.",
-        'quantity': '1',
-        'image': 'Market/scream.jpg',
-        'price': '4300'}, {
-        'pk': 5,
-        'name': 'The Kiss',
-        'description': "The Kiss (in German Der Kuss) is an oil-on-canvas painting with added gold leaf, silver and platinum, by the Austrian Symbolist painter Gustav Klimt. It was painted at some point in 1907 and 1908, during the height of what scholars call his 'Golden Period'.",
-        'quantity': '11',
-        'image': 'Market/thekiss.jpg',
-        'price': '1000'},
-    ]
     context = {
         'category': "Paintings",
         'items': models.Product.objects.filter(category="PAINTING", quantity__gt=0),
