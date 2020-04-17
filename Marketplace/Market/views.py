2d44d3ec4305c87e30909052dd5443dfdfd9930b
Olivier Grenier-Leboeuf <olivier.grenier.leboeuf@gmail.com>
2020-04-17 16:16:52 -0400

    small fixes, fixed no_product messages

--- Marketplace/Market/views.py
+++ Marketplace/Market/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from . import models
-from . import forms
 from Users import models as user_models
 from django.http import HttpResponseRedirect
 from django.contrib import messages
